9303e2c2436f7ea0e3cea7f0646ecb35329a90da
lampman <lampmanyao@gmail.com>
2025-09-04 11:29:51 +0800

    Update casts to v1.6.2

--- Casks/xanyd.rb
+++ Casks/xanyd.rb
@@ -1,7 +1,7 @@
 cask "xanyd" do
-  version "1.6.1"
-  sha256 "5e88264c4f8769831956ba7d7d98b595f39812968ef2aedce8a03a08493ef1b3"
-  url "https://github.com/XanyApp/Xanyd/releases/download/v1.6.1/Xanyd_v1.6.1.dmg"
+  version "1.6.2"
+  sha256 "5fd8fe5b90342b78ab87d79f0cd9e6e192f2a4eb4a045f626789599b025bc718"
+  url "https://github.com/XanyApp/Xanyd/releases/download/v1.6.2/Xanyd_v1.6.2.dmg"
   name "Xanyd"
   desc "An alternative VNC server."
   homepage "https://xany.app/xanyd.html"
