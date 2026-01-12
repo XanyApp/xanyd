5cbd55dfa732fac2e393e826aaa2e56f4bcc1596
lampman <lampmanyao@gmail.com>
2026-01-13 00:47:58 +0800

    Fix sha256 hash format in xanyd.rb

--- Casks/xanyd.rb
+++ Casks/xanyd.rb
@@ -1,6 +1,6 @@
 cask "xanyd" do
   version "1.7.0"
-  sha256 "sha256:48a7acaa6b0c91372452e3d2547473b886848a9a8eb34591c11207a3c89d1931"
+  sha256 "48a7acaa6b0c91372452e3d2547473b886848a9a8eb34591c11207a3c89d1931"
   url "https://github.com/XanyApp/Xanyd/releases/download/v1.7.0/Xanyd_v1.7.0.dmg"
   name "Xanyd"
   desc "An alternative VNC server."
