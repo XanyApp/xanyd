2c299405821c36f2f4af9a83ba6467d15c4d986e
lampman <lampmanyao@gmail.com>
2026-01-13 00:46:55 +0800

    Update Xanyd to version 1.7.0

--- Casks/xanyd.rb
+++ Casks/xanyd.rb
@@ -1,9 +1,9 @@
 cask "xanyd" do
-  version "1.6.2"
-  sha256 "5fd8fe5b90342b78ab87d79f0cd9e6e192f2a4eb4a045f626789599b025bc718"
-  url "https://github.com/XanyApp/Xanyd/releases/download/v1.6.2/Xanyd_v1.6.2.dmg"
+  version "1.7.0"
+  sha256 "sha256:48a7acaa6b0c91372452e3d2547473b886848a9a8eb34591c11207a3c89d1931"
+  url "https://github.com/XanyApp/Xanyd/releases/download/v1.7.0/Xanyd_v1.7.0.dmg"
   name "Xanyd"
   desc "An alternative VNC server."
-  homepage "https://xany.app/xanyd.html"
+  homepage "https://xany.app"
   app "Xanyd.app"
 end
